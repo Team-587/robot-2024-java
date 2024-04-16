917090c4b4f4c3dd534faee00885af1944f3f884
hedgehogs@team587.org <hedgehogs@team587.org>
2024-04-15 20:06:05 -0400

    using interface

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -7,12 +7,13 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ShooterSubsystem extends SubsystemBase {
 
-  CANSparkMax m_shooterMotor = new CANSparkMax(Constants.OperatorConstants.kShooterPort, CANSparkLowLevel.MotorType.kBrushless);
+  MotorController m_shooterMotor = new CANSparkMax(Constants.OperatorConstants.kShooterPort, CANSparkLowLevel.MotorType.kBrushless);
 
   /** Creates a new ShooterSubsystem. */
   public ShooterSubsystem() {}
