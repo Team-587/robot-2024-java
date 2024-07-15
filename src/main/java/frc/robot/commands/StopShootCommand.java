b1beaf09f4ebcd1ae1af519bd5cea38e6fe5c712
Tristan Tiani <djtiani@earthlink.net>
2024-07-15 13:20:03 -0400

    Updated stop shoot command
    adfa

--- src/main/java/frc/robot/commands/StopShootCommand.java
+++ src/main/java/frc/robot/commands/StopShootCommand.java
@@ -5,16 +5,22 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ShooterSubsystem;
 
 public class StopShootCommand extends Command {
+  private final ShooterSubsystem m_ShooterSubsystem;
   /** Creates a new StopShootCommand. */
-  public StopShootCommand() {
+  public StopShootCommand(ShooterSubsystem shooterSubsystem) {
+    m_ShooterSubsystem = shooterSubsystem;
+      addRequirements(m_ShooterSubsystem);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_ShooterSubsystem.setVelocity(0);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
